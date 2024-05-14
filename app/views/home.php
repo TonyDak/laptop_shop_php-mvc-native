d9a78f27eabcb67928cb4dbe876a15eba1bf326a
Duc Nguyen <duckg2083@gmail.com>
2024-05-14 20:17:03 +0700

    .

--- app/views/home.php
+++ app/views/home.php
@@ -11,29 +11,16 @@
             box-sizing: border-box;
         }
     </style>
-    <link rel="stylesheet" href="assets/css/header.css" />
     <link rel="stylesheet" href="assets/css/main.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
     <?php
     require_once 'app/views/includes/header.php';
-    require_once 'app/views/home/main.php';
+    require_once 'app/views/home/main_product_all.php';
+    require_once 'app/views/includes/footer.php';
     ?>
 </body>
-
-<script src="assets/js/check.js"></script>
-
-<!-- Bootstrap core JavaScript-->
-<script src="assets/startbootstrap-sb-admin-2-gh-pages/vendor/jquery/jquery.min.js"></script>
-<script src="assets/startbootstrap-sb-admin-2-gh-pages/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="assets/startbootstrap-sb-admin-2-gh-pages/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="assets/startbootstrap-sb-admin-2-gh-pages/js/sb-admin-2.min.js"></script>
 </html>
 
---
+++ app/views/includes/footer.php
@@ -0,0 +1,53 @@
+<footer>
+    <div class="head-footer">
+        <div>
+            <p style="font-size:xx-large;" >Nhận thông tin khuyến mãi mới nhất</p>
+            <p>Để lại email của bạn để nhận những thông tin khuyến mãi mới nhất từ chúng tôi</p>
+        </div>
+        
+        <form action="newsletter.php" method="post">
+            <input class="email" type="email" name="email" placeholder="Enter your email address">
+            <input class="btnsubmit" type="submit" value="Subscribe">
+        </form>
+    </div>
+
+    <div class="content-footer" >
+        <div>
+            <ul>
+            <p>Thông tin hữu ích</p>
+                <li><a href="#">Giới thiệu công ty</a></li>
+                <li><a href="#">Góp ý, khiếu nại</a></li>
+                <li><a href="#">Tuyển dụng</a></li>
+            </ul>
+        </div>
+        <div>         
+            <ul>
+            <p>Dịch vụ của chúng tôi</p>
+                <li><a href="#">Tra cứu thông tin bảo hành</a></li>
+                <li><a href="#">Hệ thống của hàng</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </div>
+        <div>               
+            <ul>
+            <p>Chính sách của chúng tôi</p>
+                <li><a href="#">Chính sách bảo hành</a></li>
+                <li><a href="#">Chính sách đổi trả</a></li>
+                <li><a href="#">Chính sách trả góp</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <p>Thông tin liên hệ</p>
+                <li>Phường 4, Quận 5, TP Hồ Chí Minh</li>
+                <li>Phone: +84 946 945 409</li>
+                <li><a href="mailto:duckg2083@gmail.com">Email: duckg2083@gmail.com</a></li>
+            </ul>
+        </div>
+    </div>
+    <hr>
+    <div class="foot-footer" >
+        <p>&copy; 2021 - All rights reserved</p>
+    </div>
+    
+</footer>
